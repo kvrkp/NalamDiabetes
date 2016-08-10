b33369e52574600269e01fe037c55b16e5a5b393
Mahesh Natarajan <umamags@gmail.com>
2016-08-10 17:16:03 +0530

    added php

--- www/js/services.js
+++ www/js/services.js
@@ -2,3 +2,17 @@
 
 /* Services */
 var diabetesServices = angular.module('diabetesServices', ['ngResource']);
+
+diabetesServices.factory('User', [function() {
+	var sdo = {
+		name: "",
+		dob: "",
+		bloodgroup: "",
+		sex: "",
+		weight: 0,
+		height: "",
+		diabetesType: "",
+		email: ""
+	};
+	return sdo;
+}]);
